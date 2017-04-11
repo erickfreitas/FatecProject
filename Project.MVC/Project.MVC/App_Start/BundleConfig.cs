664f0e0c1428a8d6e273da1f20c1e922cfdc2d1f
Erick Freitas <erickfreitasbezerra@gmail.com>
2017-04-11 12:57:14 -0300

    Adicionando StoreTemplate ao Projeto

--- Project.MVC/Project.MVC/App_Start/BundleConfig.cs
+++ Project.MVC/Project.MVC/App_Start/BundleConfig.cs
@@ -1,11 +1,9 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Project.MVC
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +12,6 @@ namespace Project.MVC
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -26,6 +22,77 @@ namespace Project.MVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // ------------------------- StoreContent ------------------------- //
+            bundles.Add(new StyleBundle("~/StoreContent/bootstrap/css").Include(
+                      "~/Content/StoreContent/bootstrap/css/bootstrap.css"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/bootstrap/script").Include(
+                      "~/Content/StoreContent/bootstrap/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/StoreContent/style/css").Include(
+                      "~/Content/StoreContent/css/style.css"));
+
+            //bundles.Add(new StyleBundle("~/StoreContent/fontawesome/css").Include(
+            //          "~/Content/StoreContent/fonts/style.css"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/footable/script").Include(
+                      "~/Content/StoreContent/js/footable.js",
+                      "~/Content/StoreContent/js/footable.sortable.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/bootstrap-touchspin/script").Include(
+                      "~/Content/StoreContent/js/bootstrap.touchspin.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/grids/script").Include(
+                      "~/Content/StoreContent/js/grids.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/hide-max-list/script").Include(
+                      "~/Content/StoreContent/js/hideMaxListItem-min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/home/script").Include(
+                      "~/Content/StoreContent/js/home.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/jquert-cycle2/script").Include(
+                      "~/Content/StoreContent/js/jquery.cycle2.min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/jquery-custom-scroll/script").Include(
+                      "~/Content/StoreContent/js/jquery.mCustomScrollbar.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/jquery-parallax/script").Include(
+                      "~/Content/StoreContent/js/jquery.parallax-1.1.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/jquery-scrollme/script").Include(
+                      "~/Content/StoreContent/js/jquery.scrollme.min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/jquery-validate/script").Include(
+                      "~/Content/StoreContent/js/jquery.validate.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/jquery-zoom/script").Include(
+                      "~/Content/StoreContent/js/jquery.zoom.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/owl-carousel/script").Include(
+                      "~/Content/StoreContent/js/owl.carousel.min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/owl-carousel/script").Include(
+                      "~/Content/StoreContent/js/owl.carousel.min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/pace/script").Include(
+                      "~/Content/StoreContent/js/pace.min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/script/script").Include(
+                      "~/Content/StoreContent/js/script.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/sidebar-nav/script").Include(
+                      "~/Content/StoreContent/js/sidebar-nav.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/skrollr/script").Include(
+                      "~/Content/StoreContent/js/skrollr.min.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/smoothproducts-thumb/script").Include(
+                      "~/Content/StoreContent/js/smoothproducts-thumb.js"));
+
+            bundles.Add(new ScriptBundle("~/StoreContent/smoothproducts/script").Include(
+                      "~/Content/StoreContent/js/smoothproducts.min.js"));
         }
     }
 }
